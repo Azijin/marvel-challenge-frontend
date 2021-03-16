8e0844a5344e33dd85cfe7575a308451b299c705
Arthur Chen <arthur@MacBook-Pro-de-Arthur.local>
2021-03-16 19:25:38 +0100

    modify the state in the history.push in the callBack onClick, the state props cannot contains function

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,27 +1,92 @@
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Card = (props) => {
   const history = useHistory();
-  const { title, picture, extension, id, pageContent } = props;
+  const {
+    title,
+    picture,
+    extension,
+    id,
+    pageContent,
+    isInFavorites,
+    favorites,
+    addFavorite,
+    removeFavorite,
+  } = props;
+
+  const [isFavorite, setIsFavorite] = useState(false);
+  const classNameFavorite = isFavorite ? "is-favorite" : "not-favorite";
+
+  useEffect(() => {
+    if (favorites) {
+      isInFavorites(id, pageContent, setIsFavorite);
+    }
+  }, [favorites]);
 
   return (
-    <div
-      className="card"
-      onClick={() => {
-        if (pageContent === "character") {
-          id && history.push(`/${pageContent}/${id}`);
-        } else if (pageContent === "comic") {
-          console.log("go to comic page");
-          history.push({ pathname: `/${pageContent}/${id}`, state: props });
-        }
-      }}
-    >
-      <div className="card-picture">
-        <img src={`${picture}/portrait_xlarge.${extension}`} alt={title} />
+    <div className="card">
+      <div
+        className="favorite-container"
+        onClick={() => {
+          if (!favorites) {
+            history.push("/signin");
+          } else {
+            if (!isFavorite) {
+              addFavorite(
+                {
+                  id: id,
+                  title: title,
+                  thumbnail: {
+                    path: picture,
+                    extension: extension,
+                  },
+                },
+                pageContent
+              );
+            } else {
+              removeFavorite(
+                {
+                  id: id,
+                  title: title,
+                  thumbnail: {
+                    path: picture,
+                    extension: extension,
+                  },
+                },
+                pageContent
+              );
+            }
+          }
+        }}
+      >
+        <FontAwesomeIcon
+          className={`favorite-icon ${classNameFavorite}`}
+          icon="heart"
+        />
       </div>
-      <div className="card-infos">
-        <div className="card-title">
-          <p>{title}</p>
+
+      <div
+        onClick={() => {
+          if (pageContent === "characters") {
+            history.push(`/character/${id}`);
+          } else if (pageContent === "comics") {
+            history.push({
+              pathname: `/comic/${id}`,
+              state: { title, picture, extension, id, pageContent },
+            });
+          }
+        }}
+      >
+        <div className="card-picture">
+          <img src={`${picture}/portrait_xlarge.${extension}`} alt={title} />
+        </div>
+        <div className="card-infos">
+          <div className="card-title">
+            <p>{title}</p>
+          </div>
         </div>
       </div>
     </div>
